4dad0607b22df42cf43cce8a6024980f706d8ed5
Yumarubu <241208@st.yoshida-g.ac.jp>
2025-09-13 00:01:07 +0900

    ログイン処理&ログアウト処理をひとまず実装、エラーは無し

--- app/src/app/Http/Controllers/AccountController.php
+++ app/src/app/Http/Controllers/AccountController.php
@@ -4,62 +4,68 @@ namespace App\Http\Controllers;
 
 use App\Models\Account;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
 
 class AccountController extends Controller
 {
     //アカウント一覧を表示する
-    public function index(Request $request){
+    public function index(Request $request)
+    {
 
         //テーブルの全てのレコードを取得
         $accounts = Account::All();
 
         //テーブルのレコード数を取得
-        $count = Account::count();
-        return view('account/index', ['accounts' => $accounts]);
-
-        //idで検索、見つからなかったら404エラー
-        $accounts = Account::findOrFail(1);
-
-        //条件を指定して取得
-        $accounts = Account::where('name', '=', 'jobi')->get();
-
-        //複数の条件を指定して取得
-        $accounts = Account::where('name', '=', 'jobi')->where('created_at', '>=', '2025-06-13')->get();
-
-//        $title = 'アカウント一覧';
+//        $count = Account::count();
+//        return view('accounts/index', ['accounts' => $accounts]);
 //
-//        $data = [[
-//            'name' => 'テストさん',
-//            'password' => '$3$3kdiei2',
-//        ],[
-//            'name' => '<h1>jobi</h1>',
-//            'password' => '$9$s#2kdie',
-//        ]];
+//        //idで検索、見つからなかったら404エラー
+//        $accounts = Account::findOrFail(1);
 //
-//        return view('accounts/index', ['title' => $title, 'accounts' => $data]);
+//        //条件を指定して取得
+//        $accounts = Account::where('name', '=', 'jobi')->get();
+//
+//        //複数の条件を指定して取得
+//        $accounts = Account::where('name', '=', 'jobi')->where('created_at', '>=', '2025-06-13')->get();
+
+        $title = 'アカウント一覧';
+
+        $data = [
+            [
+                'name' => 'テストさん',
+                'password' => '$3$3kdiei2',
+            ],
+            [
+                'name' => '<h1>jobi</h1>',
+                'password' => '$9$s#2kdie',
+            ]
+        ];
+
+        return view('accounts/index', ['title' => $title, 'accounts' => $data]);
     }
 
     //ログイン画面を表示する
-    public function login(){
+    public function login()
+    {
         return view('login');
     }
 
-    public function dologin(Request $request){
+    public function dologin(Request $request)
+    {
         //フォームから送られた値を取得
 //        $passName = 'jobi';
 //        $passPassWord = 'jobi';
+        $error = 'ログインに失敗しました';
 
         $name = $request->input('name');
         $password = $request->input('password');
 
         //次回はここを修正すること
-        if ($name === 'jobi' && $password === 'jobi')
-        {
+        if ($name === 'jobi' && $password === 'jobi') {
             return redirect('accounts/index');
-        }
-        else
-        {
-            return redirect('/', ['error' => 'ログインに失敗しました']);
+        } else {
+            //return redirect('/', 'error' => $error)->;
+            return redirect('/');
         }
     }
 }
--- app/src/resources/views/accounts/index.blade.php
+++ app/src/resources/views/accounts/index.blade.php
@@ -2,17 +2,21 @@
 <body>
 <h1>・{{$title}}</h1>
 
-<ul>
-    <!--<a href="../users/index">ユーザー一覧はこちら</a>
-    <br>
-    <a href="../score/index">スコア一覧はこちら</a>-->
+    <ul>
+        <!--<a href="../users/index">ユーザー一覧はこちら</a>
+        <br>
+        <a href="../score/index">スコア一覧はこちら</a>-->
 
-    @foreach($accounts as $account)
-        <li>名前:{{$account['name']}}　パス:{{$account['password']}}</li>
-    @endforeach
-    <br><br>
+        @foreach($accounts as $account)
+            <li>名前:{{$account['name']}}　パス:{{$account['password']}}</li>
+        @endforeach
 
-    <!--<a href="../">[ログアウト]</a>-->
-</ul>
+        <br><br>
+        <form method="get" action="{{url('/')}}">
+        <button type="submit" name="login">ログアウト</button>
+        <!--<a href="../">[ログアウト]</a>-->
+        </form>
+    </ul>
+
 </body>
 </html>
