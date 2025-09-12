7d6faf4c02e2c7f82d9bd057e87ff42c54cc5258
Yumarubu <241208@st.yoshida-g.ac.jp>
2025-09-13 03:13:33 +0900

    所持アイテムを管理するデータベースを追加

---
+++ app/src/database/migrations/2025_09_13_025556_have_item.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('have_item', function (Blueprint $table) {
+            $table->id();           //idカラム
+            $table->string('item_name',20);
+            $table->string('type',20);
+            $table->integer('effect');
+            $table->integer('have_count');
+            $table->timestamps();   //created_at,updated_at
+
+            //$table->index('name');      //nameにインデックス設定
+            $table->unique('item_name');     //nameにユニーク設定
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('have_item');
+    }
+};
